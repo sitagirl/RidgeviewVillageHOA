338f0a5ce94e81db8fb2173fd3031087709164a4
sitagirl <sgehani@dc.rr.com>
2016-02-16 16:37:21 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$host="localhost"; // Host name 
-	$username="sgissolu_RVHOA"; // Mysql username 
+	$username="RVHOA"; // Mysql username 
 	$password="HOAAdm1n1strat0r"; // Mysql password 
-	$db_name="sgissolu_RidgeviewHomeowners"; // Database name 
+	$db_name="RidgeviewHomeowners"; // Database name 
 	$tbl_name="members"; // Table name
 ?>
